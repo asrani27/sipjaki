cdb3b7a6828ae73900887eee41014c224270dc8f
asrani <asrani.27@gmail.com>
2024-07-02 09:56:54 +0800

    f

---
+++ resources/views/pemanfaatan.blade.php
@@ -0,0 +1,5 @@
+@extends('frontend.app3')
+@section('content')
+<h2>Tertib Pemanfaatan</h2>
+@endsection
+
---
+++ resources/views/penyelenggaraan.blade.php
@@ -0,0 +1,5 @@
+@extends('frontend.app3')
+@section('content')
+<h2>Tertib Penyelenggaraan</h2>
+@endsection
+
